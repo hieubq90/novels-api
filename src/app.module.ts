8f041367bd2866466af6f1066c958330830cd327
hieubq <hieubq>
2024-04-02 12:12:25 +0700

    update index

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
+import { join } from 'path'
+
 import { Module } from '@nestjs/common'
+import { ServeStaticModule } from '@nestjs/serve-static'
 
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { GenresModule } from './genres/genres.module'
 import { NovelsModule } from './novels/novels.module'
-import { GenresModule } from './genres/genres.module';
 
 @Module({
-  imports: [NovelsModule, GenresModule],
+  imports: [ServeStaticModule.forRoot({
+    rootPath: join(__dirname, '..', 'client'),
+    serveRoot: "",
+    exclude: ['/api/(.*)'],
+  }), NovelsModule, GenresModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -18,5 +18,6 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
